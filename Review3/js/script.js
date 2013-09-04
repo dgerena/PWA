791baa20e7bcb1c36bd73ed89feba14c4103ea29
Eli <temp@temps-MacBook-Pro.local>
2013-09-04 12:36:34 -0400

    reviews

--- Review3/js/script.js
+++ Review3/js/script.js
@@ -12,9 +12,10 @@ function calcArea(height, width){
 //console.log(a);
 
 //Anonymouse Funciton --------------------------------------
-calcForce(6,7);
+//calcForce(6,7);// the broswer wil not be able to find the function in a anonymouse function. Dost thou even hoist? no sir "anonymouse"
 
 var calcForce = function(mass, acc){
 	var force = mass*acc;
 	console.log(force);
 }
+calcForce(6,7);
