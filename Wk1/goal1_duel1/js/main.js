2ce4ffaf1df1b2aaf61879084aeb4441aee48551
Eli <temp@temps-MacBook-Pro.local>
2013-09-04 14:28:58 -0400

    fight funciton
    getting in the information as i go form the players to the formulas

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -11,8 +11,31 @@
 	var p1Dmg=20;
 	var p2Dmg=20;
 	//player health
-	var playerOneHealth
+	var p1Hp=100;
+	var p2Hp=100;
+	//number of rounds
+	var rndNum=1;
+	function fight(){
+		console.log("in the fight function")
+		 alert(p1Name+":"+p1Hp+" *START* "+p2Name+":"+p2HP)
+		for(var i=0;i<10;i++){
+		//random number formula for a min of half damage-math.floor(Math.random()*(50-25)+25);
+			console.log(i)
+			var minDmg1=p1Dmg*.5;
+			var minDmg2=p2Dmg*.5;
+		}
+		winnerCheck()
+	};
+	function winnerCheck(){
+		console.log("win cond. checked.")
+	};
+	
+	console.log("begining program")
+	fight()
+	
 })();
+
+
 
 
 
