0568001bb213968c3653d3bc8d376a781728d3f9
Eli <temp@temps-MacBook-Pro.local>
2013-09-04 14:12:04 -0400

    pulled files from fso and began creating and editign js

---
+++ Wk1/goal1_duel1/js/main.js
@@ -0,0 +1,20 @@
+//Damasio Eli Gerena IV
+//Wed Sept. 4th, 2013
+//Programing web applications
+//Assignmnt:goal 1 assignment: duel 1
+(function(){
+	console.log("Fight!")
+	//player name
+	var p1Name="Kickass";
+	var p2Name="YourMom";
+	//player damage
+	var p1Dmg=20;
+	var p2Dmg=20;
+	//player health
+	var playerOneHealth
+})();
+
+
+
+
+console.log("working")
