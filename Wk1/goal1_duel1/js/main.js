a20ce1ab65e017d3f79712367822cef6330bdcdc
Eli <temp@temps-MacBook-Pro.local>
2013-09-06 10:54:11 -0400

    added in array eddited
     changed the code for all placement of initial variables

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -4,6 +4,9 @@
 //Assignmnt:goal 1 assignment: duel 1
 (function(){
 	//console.log("Fight!")
+	//array literal
+	var ftr1=["Kickass",20,100];
+	var ftr2=["Your Mom",20,100];
 	//player name
 	var p1Name="Kickass";
 	var p2Name="Your Mom";
@@ -13,11 +16,37 @@
 	//player health
 	var p1Hp=100;
 	var p2Hp=100;
+	console.log(ftr1[0],ftr1[1],ftr1[2]);
 	//number of rounds
 	var rndNum=0;
 	function fight(){
 		//console.log("in the fight function")
-		 alert(p1Name+":"+p1Hp+" *START* "+p2Name+":"+p2Hp)
+		alert(ftr1[0]+":"+ftr1[2]+" *START* "+ftr2[0]+":"+ftr2[2])
+		for(var i=0;i<10;i++){
+		//random number formula for a min of half damage-math.floor(Math.random()*(50-25)+25);
+			console.log(i)
+			var minDmg1=ftr1[1]*.5;
+			var minDmg2=ftr2[1]*.5;
+			var f1=Math.floor(Math.random()*(ftr1[1]-minDmg1)+minDmg1)
+			var f2=Math.floor(Math.random()*(ftr2[1]-minDmg2)+minDmg2)
+			//dmg done
+			ftr1[2]-=f1;
+			ftr2[2]-=f2;
+			console.log(ftr1[0]+":"+ftr1[2]+" "+ftr2[0]+":"+ftr2[2]);
+			var results=winnerCheck();
+			console.log(results)
+			if(results==="No Winner"){
+				rndNum++;
+				alert(ftr1[0]+":"+ftr1[2]+"*Round "+rndNum+" Over*"+ftr2[0]+":"+ftr2[2])
+			}else{
+				alert(results);
+				break;
+			};
+			//else if
+		};
+	/*function fight(){
+		//console.log("in the fight function")
+		alert(p1Name+":"+p1Hp+" *START* "+p2Name+":"+p2Hp)
 		for(var i=0;i<10;i++){
 		//random number formula for a min of half damage-math.floor(Math.random()*(50-25)+25);
 			console.log(i)
@@ -39,17 +68,17 @@
 				break;
 			};
 			//else if
-		};
-	} ;
+		};*/
+	};
 	function winnerCheck(){
 	//console.log("checked")
 		var result="No Winner";
-		if(p1Hp<1 && p2Hp<1){
+		if(ftr1[2]<1 && ftr2[2]<1){
 			result="You Both Die";	
-		}else if(p1Hp<1){
-			result=p2Name+" Wins!!!";
+		}else if(ftr1[2]<1){
+			result=ftr2[0]+" Wins!!!";
 		}else if(p2Hp<1){
-			result=p1Name+" Wins!!!";
+			result=ftr1[0]+" Wins!!!";
 		};
 		return result
 		//console.log("win cond. checked.")
@@ -59,4 +88,4 @@
 	fight()
 	
 })();
-//console.log("working")
+//console.log("working");
