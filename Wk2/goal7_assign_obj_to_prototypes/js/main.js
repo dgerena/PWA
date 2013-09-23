7c35f74428f893e83271f21fef4a61c545d95f72
Eli <temp@temps-MacBook-Pro.local>
2013-09-22 23:53:08 -0400

    cannot figure out whats wrong with person not being presented in window.
    sill me got help and still couldnt get it….

--- Wk2/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk2/goal7_assign_obj_to_prototypes/js/main.js
@@ -6,10 +6,10 @@
 (function(){
 	var time=setInterval(update, 1000/1);
 	var people=["jim","jam","jom","jib"];
-	var prsn=new Person(name);
-	prsn.name();
-	prsn.job();
-	prsn.action();
+	var person=new Person(name);
+	person.name();
+	person.job();
+	person.action();
 	//people.push(person1,person2,person3,person4);
 /*
 	// example of cleaner way to creat objects and put them into the array *people.push(new Person("dfds","asfdaf"));*
@@ -18,8 +18,8 @@
 	};
 */
 	function update(){//find a random person in the array, then display there job.
-		prsn.name();
-		prsn.job();
-		prsn.action();
+		person.name();
+		person.job();
+		person.action();
 	};
 })();
--- Wk2/goal7_assign_obj_to_prototypes/js/person.js
+++ Wk2/goal7_assign_obj_to_prototypes/js/person.js
@@ -10,7 +10,7 @@
  		this.actions=["sleeping","eating","farting","dieting"];
  		console.log(this.name+" created.");
  	};
- 	person.prototype.name=function(){
+ 	Person.prototype.name=function(){
  		var self=this;
  		var ppl=[];
  		nmeRndm();
@@ -26,16 +26,39 @@
 			document.getElementById("r3c1").innerHTML=people[2];
  		};
  	};
- 	person.prototype.job=function(){
+ 	Person.prototype.job=function(){
 		var self=this;
 		var specJob=[];
 		rndmJob();
 		function rndmJob(){
-			
+			for(i=0;i<3;i++){
+				specJob.push(self.job[(~~(Math.random()*5))]);
+			};
+			clm2();
+		};
+		function clm2(){
+			document.getElementById("r1c2").innerHTML=jb[0];
+			document.getElementById("r2c2").innerHTML=jb[1];
+			document.getElementById("r3c2").innerHTML=jb[2];
 		};
  	};
- 	
- 	window.Person=Person;
+ 	Person.prototype.action=function(){
+	 	var self=this;
+	 	var actn=[];
+	 	rndmActn();
+	 	function rndmActn(){
+		 	for(I=3;i<3;i++){
+			 	actn.push(self.action[(~~(Math.random()*5))]);
+		 	};
+		 	clm3()
+	 	};
+	 	function clm3(){
+		 	document.getElementById("r1c3").innerHTML=act[0];
+			document.getElementById("r2c3").innerHTML=act[1];
+			document.getElementById("r3c3").innerHTML=act[2];
+	 	};
+ 	};
 
+window.Person=Person;
  	
- })();
+})();
